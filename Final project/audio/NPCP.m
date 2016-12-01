a7aa871b5ff59a67b62fc05bfefd19b7650583ab
Jay Huang <kingsleyzhijiehuang@gmail.com>
2016-12-01 16:21:35 -0700

    Using the maximum threshold for the entire song

--- Final project/audio/NPCP.m
+++ Final project/audio/NPCP.m
@@ -1,4 +1,4 @@
-function [ output ] = NPCP( wav,fs,fftSize,window)
+function [ output ] = NPCP( wav,fs,fftSize,window,Threshold)
 %
 % PART 2 : processing of the audio vector In the Fourier domain.
 %_________________________________________________________
@@ -6,11 +6,15 @@ function [ output ] = NPCP( wav,fs,fftSize,window)
    xn = wav(1:fftSize);
    Y = fft(xn.*window);
    K = fftSize/2+1;
+   L = K-1;
    Xn = abs(Y(1:K));
+ %-------------Plot of the fourier transform--------------------------
+%    f = fs*(0:L)/L;
+%    plot(f,Xn);
 %--------Step 1 pick out the peak frequencies-------
 f0 = 27.5;
 %------Goal of the threshold:Get rid of the small amplitude note----
-[pkt,fk] = findpeaks(Xn,'Threshold',max(Xn)*1e-2);
+[pkt,fk] = findpeaks(Xn,'Threshold',Threshold*1e-3);
  FK = round(fk*fs/fftSize);
 %---------Step 2: Assignment of peak frequencies to 
 %-----------------------------semitones-------------------------  
@@ -23,17 +27,5 @@ f0 = 27.5;
        w = (cos(pi/2*r)).^2;
        output(j,1) = sum(var.*(w.*pkt.^2));
    end
-%        for c = 1:12
-% %--------Bring everything into the first octa
-%        r = 12*log2(fk/f0) - sm; 
-% %----------------Step 3:the pitch class profile:a weighted
-%        if abs(r) < 1 & c == rem(sm,12);
-%             w = (cos(pi/2*(r))).^2;
-%             output(i,c) = w.*(pkt.^2);
-%        else
-%            output(i,c) = 0;
-%        end
-%        end
-
 end
 
